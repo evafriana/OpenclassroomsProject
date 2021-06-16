b4c846e0e396106ed40ca1bc6538251b30757940
Eva Afriana <vava.afriana@gmail.com>
2021-06-16 21:57:55 +0200

    create model photographer page mobile

---
+++ EvaAfriana_6_14062021/js/photographerPage.js
@@ -0,0 +1,9 @@
+mainProfil.addEventListener("click", (event) => {
+  // for (let i = 0; i < photographers.length; i++) {
+  //     const profil = document.getElementBy("photographer__profil");
+  //     const div = document.createElement("div");
+  //     div.innerHTML = `
+  //       <p>Hello</p>
+  //       `;
+  //   };
+});
